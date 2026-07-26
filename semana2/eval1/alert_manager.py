d7f485b31053ddf253eede4d731193580adf8175
JafetPalacios <jaft9810@gmail.com>
2026-07-25 21:49:51 -0600

    feat: implementar delegación de alertas

---
+++ semana2/eval1/alert_manager.py
@@ -0,0 +1,25 @@
+"""Administración de alertas mediante estrategias intercambiables"""
+
+from abc import ABC, abstractmethod
+
+from semana2.eval1.anomaly_detector import Anomaly
+
+
+class AlertStrategy(ABC):
+    """Contrato para las estrategias de envío de alertas"""
+
+    @abstractmethod
+    def send(self, anomaly: Anomaly) -> None:
+        """Envía una anomalía mediante un mecanismo específico"""
+
+
+class AlertManager:
+    """Delega el envío de alertas a una estrategia inyectada"""
+
+    def __init__(self, strategy: AlertStrategy) -> None:
+        """Configura la estrategia utilizada para enviar alertas"""
+        self._strategy = strategy
+
+    def send(self, anomaly: Anomaly) -> None:
+        """Envía una anomalía usando la estrategia configurada"""
+        self._strategy.send(anomaly)
